e9b318e58535ab316803142bb7d50da58eb9a426
William Porto <william.porto@nave.rs>
2021-11-04 09:18:24 -0300

    chore(scripts): adding some improvements

--- setup.sh
+++ setup.sh
@@ -1,22 +1,23 @@
 #! /bin/sh
 
-read -p "Project name: " PROJECT_NAME
+read -p "[1/6] >> Type the project name: " PROJECT_NAME
 
-echo "[1/4] >> Creating a new project based on naveteam/flutter-boilerplate."
+echo "[2/6] >> Creating a new project based on naveteam/flutter-boilerplate."
 git clone -b main https://github.com/naveteam/flutter-boilerplate $PROJECT_NAME
 
-echo "[2/4] >> Initializing a new git repository."
+echo "[3/6] >> Initializing a new git repository."
 cd $PROJECT_NAME
 rm -rf .git
 git init --initial-branch=main
-
-echo $(cat .env.example) >> .env
+cp .env.example .env
 
 if ! [ -x "$(command -v flutter)" ]; then
   echo "Flutter SDK wasn't found in your PATH."
 else
-  echo "[3/4] >> Installing dependencies."
+  echo "[4/6] >> Installing dependencies."
   flutter pub get
-  echo "[4/4] >> Running code-generation tool."
+  echo "[5/6] >> Running code-generation tool."
   flutter pub run build_runner build --delete-conflicting-outputs
+  echo "[6/6] >> Checking if there's an update available for all dependencies. Try to update before pushing for the first time."
+  flutter pub outdated
 fi
