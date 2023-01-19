fc58ae155c65ef7dbd51ac6d19af62365cacab04
William <william.porto@nave.rs>
2023-01-19 20:44:57 -0300

    chore(deps): upgrading dependencies

--- lib/app/di/application_container.dart
+++ lib/app/di/application_container.dart
@@ -4,11 +4,11 @@ import 'package:flutter_boilerplate/app/domain/http/services/authentication_serv
 import 'package:flutter_boilerplate/app/domain/persistence/credentials_persistence.dart';
 import 'package:flutter_boilerplate/app/domain/repositories/authentication_repository.dart';
 import 'package:flutter_boilerplate/app/presentation/viewmodels/authentication/authentication_viewmodel.dart';
-import 'package:flutter_boilerplate/env/env.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 abstract class ApplicationContainer {
-  static final Dio client = Dio(BaseOptions(baseUrl: Env.baseUrl))
+  static final Dio client = Dio(BaseOptions(baseUrl: dotenv.env["BASE_URL"]!))
     ..interceptors.addAll([
       AuthenticationInterceptor(credentialsPersistence),
     ]);
@@ -20,7 +20,7 @@ abstract class ApplicationContainer {
       AndroidOptions(encryptedSharedPreferences: true);
 
   static const IOSOptions secureStorageIOSOptions =
-      IOSOptions(accessibility: IOSAccessibility.first_unlock);
+      IOSOptions(accessibility: KeychainAccessibility.first_unlock);
 
   static const CredentialsPersistence credentialsPersistence =
       CredentialsPersistenceImpl(
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/app/di/application_container.dart';
 import 'package:flutter_boilerplate/app/presentation/application.dart';
 import 'package:flutter_boilerplate/app/presentation/viewmodels/authentication/authentication_viewmodel.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider<AuthenticationViewModel>(
-            create: (context) => ApplicationContainer.authenticationViewModel,
-          ),
-        ],
-        child: const Application(),
-      ),
-    );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationViewModel>(
+          create: (context) => ApplicationContainer.authenticationViewModel,
+        ),
+      ],
+      child: const Application(),
+    ),
+  );
+}
