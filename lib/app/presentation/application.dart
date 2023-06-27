4c038c71ff002f30cadb041f01714133da93c054
William <william.porto@nav9.tech>
2023-06-27 19:01:48 -0300

    deprecate: SystemOverlayBuilder

--- lib/app/presentation/application.dart
+++ lib/app/presentation/application.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/app/presentation/navigation.dart';
 import 'package:flutter_boilerplate/app/presentation/theme/theme.dart';
-import 'package:flutter_boilerplate/app/presentation/widgets/builders.dart';
 
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
@@ -12,7 +11,6 @@ class Application extends StatelessWidget {
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       routerConfig: Navigation.config,
-      builder: (context, child) => SystemOverlayBuilder(builder: (context) => child!),
     );
   }
 }
--- lib/app/presentation/widgets/builders.dart
+++ lib/app/presentation/widgets/builders.dart
@@ -1,51 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_boilerplate/app/presentation/theme/colors.dart';
-import 'package:flutter_boilerplate/app/utils/extensions.dart';
 import 'package:provider/provider.dart';
-
-class SystemOverlayBuilder extends StatefulWidget {
-  final WidgetBuilder builder;
-
-  const SystemOverlayBuilder({
-    Key? key,
-    required this.builder,
-  }) : super(key: key);
-
-  @override
-  State<SystemOverlayBuilder> createState() => _SystemOverlayBuilderState();
-}
-
-class _SystemOverlayBuilderState extends State<SystemOverlayBuilder> with WidgetsBindingObserver {
-  @override
-  Widget build(BuildContext context) => widget.builder(context);
-
-  @override
-  void didChangePlatformBrightness() {
-    super.didChangePlatformBrightness();
-    _handle();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    _handle();
-  }
-
-  void _handle() {
-    final brightness = SchedulerBinding.instance.platformDispatcher.platformBrightness;
-    final surfaceColor = brightness.isDarkModeEnabled ? DarkColors.surface : LightColors.surface;
-
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        systemNavigationBarIconBrightness: brightness.inverse,
-        systemNavigationBarColor: surfaceColor,
-      ),
-    );
-  }
-}
 
 class ViewModelBuilder<T extends ChangeNotifier> extends StatelessWidget {
   final T Function(BuildContext) create;
